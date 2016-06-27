97577bc8d42b25c316c39a3158f7d0dfde29bf02
Chris Atkin <atkinchris@gmail.com>
2016-06-27 11:48:14 +0100

    Add Exercise 2 answer

--- exercises/ex2/ex2.js
+++ exercises/ex2/ex2.js
@@ -1,10 +1,12 @@
-function foo() { }
+function foo(a1, x, y, ...a2) {
+	return [a1, ...a2];
+}
 
 function bar() {
 	var a1 = [ 2, 4 ];
 	var a2 = [ 6, 8, 10, 12 ];
 
-	return foo();
+	return foo(...a1, ...a2);
 }
 
 console.log(
